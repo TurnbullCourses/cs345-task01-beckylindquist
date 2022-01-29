c4cd6a944cea447f2ac3a7cdcf00a8aab4c1f368
Becky Lindquist <beckylind1uist2018@gmail.com>
2022-01-29 10:15:42 -0600

    Withdraw Neg Funds Exception, added tests for withdraw and get balance

--- src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
@@ -9,17 +9,29 @@ class BankAccountTest {
     @Test
     void getBalanceTest() {
         BankAccount bankAccount = new BankAccount("a@b.com", 200);
-
         assertEquals(200, bankAccount.getBalance(), 0.001);
+
+        BankAccount bankAccountZero = new BankAccount("b@c.com", 0);
+        assertEquals (0, bankAccountZero.getBalance(), 0.001);
     }
 
     @Test
     void withdrawTest() throws InsufficientFundsException{
-        BankAccount bankAccount = new BankAccount("a@b.com", 200);
+        BankAccount bankAccount = new BankAccount("a@b.com", 200); //withdraw some money
         bankAccount.withdraw(100);
 
         assertEquals(100, bankAccount.getBalance(), 0.001);
-        assertThrows(InsufficientFundsException.class, () -> bankAccount.withdraw(300));
+        assertThrows(InsufficientFundsException.class, () -> bankAccount.withdraw(300)); //withdrawing more than is in account
+
+        bankAccount.withdraw(100); //withdraw all funds
+        assertEquals(0, bankAccount.getBalance(), 0.001);
+
+        bankAccount.withdraw(0); //withdraw zero funds
+        assertEquals(0, bankAccount.getBalance(), 0.001);
+
+        bankAccount.withdraw(-1); //withdraw negative funds
+        assertThrows(WithdrawNegativeFundsException.class, () -> bankAccount.withdraw(-1)); //throws new exception
+
     }
 
     @Test
