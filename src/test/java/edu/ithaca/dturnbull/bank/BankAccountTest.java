6f5e4dc538e0865c8d6d16c254f4b0be097e2eed
Becky Lindquist <beckylind1uist2018@gmail.com>
2022-01-31 00:10:59 -0500

    Added getBalance and withdraw tests

--- src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
@@ -10,9 +10,6 @@ class BankAccountTest {
     void getBalanceTest() {
         BankAccount bankAccount = new BankAccount("a@b.com", 200);
         assertEquals(200, bankAccount.getBalance(), 0.001);
-
-        BankAccount bankAccountZero = new BankAccount("b@c.com", 0);
-        assertEquals (0, bankAccountZero.getBalance(), 0.001);
     }
 
     @Test
@@ -20,18 +17,16 @@ class BankAccountTest {
         BankAccount bankAccount = new BankAccount("a@b.com", 200); //withdraw some money
         bankAccount.withdraw(100);
 
-        assertEquals(100, bankAccount.getBalance(), 0.001);
-        assertThrows(InsufficientFundsException.class, () -> bankAccount.withdraw(300)); //withdrawing more than is in account
+        bankAccount.withdraw(0); //withdraw zero money
+        assertEquals(200, bankAccount.getBalance());
 
-        bankAccount.withdraw(100); //withdraw all funds
+        bankAccount.withdraw(200); //withdraw all funds
         assertEquals(0, bankAccount.getBalance(), 0.001);
 
-        bankAccount.withdraw(0); //withdraw zero funds
-        assertEquals(0, bankAccount.getBalance(), 0.001);
-
-        bankAccount.withdraw(-1); //withdraw negative funds
-        assertThrows(WithdrawNegativeFundsException.class, () -> bankAccount.withdraw(-1)); //throws new exception
-
+        assertEquals(100, bankAccount.getBalance(), 0.001); //withdraw more than is in account
+        assertThrows(InsufficientFundsException.class, () -> bankAccount.withdraw(300));
+        
+        assertThrows(NegativeWithdrawException.class, () -> bankAccount.withdraw(-1)); //withdraw negative money
     }
 
     @Test
@@ -46,14 +41,12 @@ class BankAccountTest {
         assertFalse(BankAccount.isEmailValid("a#b@c.com")); //no special chars
 
         //Domain Tests
-        assertFalse(BankAccount.isEmailValid("b.com")); //just domain
+        assertFalse(BankAccount.isEmailValid("@b.com")); //no prefix
         assertFalse(BankAccount.isEmailValid("a@b")); //no domain end
         assertFalse(BankAccount.isEmailValid("a@b.c")); //no domain with only 1 character
         assertFalse(BankAccount.isEmailValid("a@@b.com")); //no two @s
         assertFalse(BankAccount.isEmailValid("a@b..com")); //no two conjunctive periods
         assertTrue(BankAccount.isEmailValid("a@b.c#om")); //no special chars
-        
-
     }
 
     @Test
